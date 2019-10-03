e0b646f41986dfc1d56867efc3888e064da2c5de
user <user@thrive-mm02.local>
2019-10-03 17:29:16 +0300

    add: part of new doc merge

--- src/document/interfaces/merged.document.data.interface.ts
+++ src/document/interfaces/merged.document.data.interface.ts
@@ -1,4 +1,7 @@
+import {IDocumentBookmark} from "./document.bookmark.interface";
+import { Element, ElementCompact} from 'xml-js';
+
 export interface IMergedDocumentData {
-    bookmarks: any[];
-    paragraphs: any[];
+    bookmarks: IDocumentBookmark[];
+    paragraphs: Element | ElementCompact;
 }
--- src/document/lib/document.merger.ts
+++ src/document/lib/document.merger.ts
@@ -1,6 +1,8 @@
 import * as xl from 'xml2js';
 import { IMergedDocumentData } from '../interfaces/merged.document.data.interface';
 import { IDocumentBookmark } from '../interfaces/document.bookmark.interface';
+import * as convert from 'xml-js';
+import { IMergedDocument } from "../interfaces/merged.document.interface";
 
 /**
  * Класс для объединения содержимого документов в один главный документ
@@ -18,29 +20,39 @@ export default class DocumentMerger {
     private documentObject: any;
     private bookmarks: IDocumentBookmark[];
     private paragraphs: any[];
+    private hyperlinks: convert.Element | convert.ElementCompact;
 
     constructor() {
         this.document = null;
         this.documentObject = null;
-        this.bookmarks = [];
-        this.paragraphs = [];
+        this.bookmarks = Array();
+        this.paragraphs = Array();
+        this.hyperlinks = null;
     }
 
     /**
      *
      */
     private checkValidateParams(): boolean {
-        if (this.document === null || this.document === '') {
-            throw Error('[checkValidateParams] Error: document == null || ""');
+        if (this.document == null || this.document == '') {
+            console.error('[CheckValidateParams] Ошибка! document == null || ""');
+            return true;
         }
-        if (this.documentObject === null || this.documentObject === '') {
-            throw Error('[checkValidateParams] Error: documentObject == null || ""');
+        if (this.documentObject == null || this.documentObject == '') {
+            console.error('[CheckValidateParams] Ошибка! document_object == null || ""');
+            return true;
         }
-        if (this.bookmarks.length <= 0 || this.bookmarks === null) {
-            throw Error('[checkValidateParams] Error: bookmarks <= 0 || null');
+        if (this.bookmarks.length <= 0 || this.bookmarks == null) {
+            console.error('[CheckValidateParams] Ошибка! bookmarks <= 0 || null');
+            return true;
         }
-        if (this.paragraphs.length <= 0 || this.paragraphs === null) {
-            throw Error('[checkValidateParams] Error: bookmarks <= 0 || null');
+        if (this.paragraphs.length <= 0 || this.paragraphs == null) {
+            console.error('[CheckValidateParams] Ошибка! bookmarks <= 0 || null');
+            return true;
+        }
+        if (this.hyperlinks.length <= 0 || this.hyperlinks == null) {
+            console.error('[CheckValidateParams] Ошибка! hyperlinks <= 0 || null');
+            return true;
         }
         return false;
     }
@@ -48,19 +60,25 @@ export default class DocumentMerger {
     /**
      * объединение всех файлов (дочерних) в главный документ
      * @param {string[]} xmlFiles Массив XML файллов (дочерних)
+     * @param {string[]} xmlLinks Массив ссылок XML файллов (дочерних)
      * @returns {void}
      */
-    public async start(xmlFiles: string[]): Promise<string> {
+    public async start(xmlFiles: string[], xmlLinks: string[]): Promise<IMergedDocument> {
         try {
             this.checkValidateParams();
-            for (const xml of xmlFiles) {
-                const response: IMergedDocumentData = await this.getOperation(this.paragraphs, this.bookmarks, xml);
-                this.bookmarks = response.bookmarks;
-                this.paragraphs = response.paragraphs;
+            for (const [index, xml] of xmlFiles.entries()) {
+                try {
+                    const response: IMergedDocumentData = await this.getOperation(this.paragraphs, this.bookmarks, xml, xmlLinks[index]);
+                    this.bookmarks = response.bookmarks;
+                    this.paragraphs = response.paragraphs;
+                } catch (error) {
+                    console.error(`[Start] Ошибка при работе с файлом. ${error}`);
+                }
             }
-            this.documentObject['w:document']['w:body'][0]['w:p'] = this.paragraphs;
-            const builder = new xl.Builder();
-            return builder.buildObject(this.documentObject);
+            this.documentObject['elements'][0]['elements'][0]['elements'] = this.paragraphs;
+            const document = convert.js2xml(this.documentObject);
+            const linksDocument = convert.js2xml(this.hyperlinks);
+            return { document, linksDocument };
         } catch (error) {
             throw Error(`[start] Ошибка при работе с файлом. ${error}`);
         }
@@ -69,25 +87,21 @@ export default class DocumentMerger {
     /**
      * Загрузить главный документ
      * @param {string} xml XML документа
+     * @param {string} links XML ссылки документа
      * @returns {void}
      */
-    public async load(xml: string): Promise<void> {
+    public async load(xml: string, links: string): Promise<void> {
         try {
-            let parseXmlFile = null;
-            await xl.parseString(
-                xml,
-                { trim: false },
-                (err, result) => {
-                    parseXmlFile = result;
-                },
-            );
+            let parseXmlFile = convert.xml2js(xml, {compact: false, trim: false});
+            this.hyperlinks = convert.xml2js(links, {compact: false, trim: false});
             this.document = xml;
             this.documentObject = parseXmlFile;
-            this.bookmarks = this.getBookmarks(xml);
-            this.paragraphs = this.documentObject['w:document']['w:body'][0]['w:p'];
+            this.bookmarks = await this.getBookmarks(xml);
+            this.paragraphs = this.documentObject['elements'][0]['elements'][0]['elements'];
             this.checkValidateParams();
-            // Фикс закладок, удаляем глобальные закладки
-            await this.deleteGlobalBookmarks();
+            const res: IMergedDocumentData = await this.deleteGlobalBookmarks(this.bookmarks, this.paragraphs);
+            this.bookmarks = res.bookmarks;
+            this.paragraphs = res.paragraphs;
         } catch (error) {
             throw error;
         }
@@ -97,27 +111,82 @@ export default class DocumentMerger {
      * Удалить глобальные закладки (гл. Документ)
      * @returns {void}
      */
-    private async deleteGlobalBookmarks(): Promise<void> {
-        if (!this.documentObject['w:document']['w:body'][0]['w:bookmarkEnd']) {
-            return;
-        }
-        const bookmarksEnd = this.documentObject['w:document']['w:body'][0]['w:bookmarkEnd'];
-        for (const [index, bookmarkEnd] of bookmarksEnd.entries()) {
-            if (!bookmarkEnd.$) {
-                continue;
-            }
-            const bookmarkEndId = bookmarkEnd.$['w:id'];
-            const bookmarkEndIndex = this.bookmarks.findIndex(i => {
-                return i && i.id === bookmarkEndId;
-            });
-            if (bookmarkEndIndex > -1) {
-                delete this.documentObject['w:document']['w:body'][0]['w:bookmarkEnd'][index];
-                if (!this.paragraphs[this.bookmarks[bookmarkEndIndex].end]['w:bookmarkEnd']) {
-                    this.paragraphs[this.bookmarks[bookmarkEndIndex].end]['w:bookmarkEnd'] = [];
+    private async deleteGlobalBookmarks(bookmarks: IDocumentBookmark[], paragraphs: convert.Element | convert.ElementCompact): Promise<IMergedDocumentData> {
+        try {
+            if (paragraphs.findIndex(i => i.name === 'w:bookmarkEnd') !== -1) {
+                for (const bookmark of bookmarks) {
+                    if (bookmark.endInOtherPR || bookmark.endIsMinePR) continue;
+                    let index = null;
+                    try {
+                        index = await paragraphs.findIndex(i => (i === null || i === undefined ? '' : i['attributes']['w:id']) === bookmark.id);
+                    } catch (error) {
+                        index = null;
+                        console.error(`[DeleteGlobalBookmarks] ${error}`);
+                    }
+
+                    if (index) {
+                        await paragraphs[bookmark.end]['elements'].push(paragraphs[index]);
+                        paragraphs[index] = null;
+                        await paragraphs.splice(index, 1);
+                        bookmark.endIsMinePR = true;
+                    }
                 }
-                this.paragraphs[this.bookmarks[bookmarkEndIndex].end]['w:bookmarkEnd']
-                    .push({ $: { 'w:id': this.bookmarks[bookmarkEndIndex].id } });
+                return { paragraphs, bookmarks}
+            } else {
+                return {paragraphs, bookmarks}
             }
+        }catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    /**
+     * Генерирование уникального ИД ссылкам и добавление ссылок в общий массив ссылок (Main документа)
+     * @param {number} start
+     * @param {number} end
+     * @param {{}[]} paragraphs
+     * @param {{}[]} links
+     * @returns {{}[]} paragraphs
+     */
+    async computingLinks(
+        start: number,
+        end: number,
+        paragraphs: convert.Element | convert.ElementCompact,
+        links: convert.Element | convert.ElementCompact
+    ): Promise<convert.Element | convert.ElementCompact> {
+        try {
+            this.checkValidateParams();
+            for(let i = start; i <= end; i++) {
+                const _max_el = paragraphs[i]['elements'].length;
+                if(_max_el < 1) continue;
+                for(let j = 0; j < _max_el; j++) {
+                    const _name_el = paragraphs[i]['elements'][j]['name'];
+                    if(_name_el != 'w:hyperlink') continue;
+                    if (
+                        paragraphs[i]['elements'][j]['attributes'] === null ||
+                        paragraphs[i]['elements'][j]['attributes'] === undefined
+                    ) {
+                        continue;
+                    }
+                    if (
+                        paragraphs[i]['elements'][j]['attributes']['r:id'] === null ||
+                        paragraphs[i]['elements'][j]['attributes'] === undefined
+                    ) {
+                        continue;
+                    }
+                    const _dt_now_stamp = new Date();
+                    const _id = paragraphs[i]['elements'][j]['attributes']['r:id'];
+                    paragraphs[i]['elements'][j]['attributes']['r:id'] = 'rId' + _dt_now_stamp.getTime().toString();
+                    const _index = links['elements'][0]['elements'].findIndex(i => i.attributes.Id == _id);
+                    links['elements'][0]['elements'][_index]['attributes']['Id'] = 'rId' + _dt_now_stamp.getTime().toString();
+                    this.hyperlinks['elements'][0]['elements'].push(links['elements'][0]['elements'][_index]);
+                }
+            }
+            return paragraphs;
+        } catch (error) {
+            console.log(error);
+            throw error;
         }
     }
 
@@ -128,7 +197,7 @@ export default class DocumentMerger {
      * @param {string} xml
      * @returns {{bookmarks: {}[], paragraphs: {}[]}}
      */
-    private async getOperation(mainParagraphs: any[], mainBookmarks: IDocumentBookmark[], xml: string): Promise<IMergedDocumentData> {
+    private async getOperation(mainParagraphs: any[], mainBookmarks: IDocumentBookmark[], xml: string, linksXml: string): Promise<IMergedDocumentData> {
         // ? #1 Избежать системных закладок и работать с закладками которые есть в обоих файлах
         // ? #2 Если количество параграфов совпадает
         // ? #3 Если в чайлд больше параграфов чем в мейн
@@ -136,18 +205,13 @@ export default class DocumentMerger {
         // !Исправить, после замены параграфов,текстов - заменить ID's на мейн
         try {
             this.checkValidateParams();
-            let objectXml = null;
+            let objectXml = await convert.xml2js(xml, {compact: false, trim: false}); // Конвертируем XML в Array of Object;
+            let objectLinksXml = await convert.xml2js(linksXml, {compact: false, trim: false}); // Конвертируем XML в Array of Object;
 
-            await xl.parseString(
-                xml,
-                { trim: false },
-                (err, result) => {
-                    objectXml = result; // Конвертируем XML в Object
-                },
-            );
-
-            const bookmarksXml: IDocumentBookmark[] = this.getBookmarks(xml); // Получаем все закладки из данного документа
-            const paragraphsXml = objectXml['w:document']['w:body'][0]['w:p']; // Достаем параграф ввиде объекта
+            // Удаляем глобальные закладки
+            const documentData: IMergedDocumentData = await this.deleteGlobalBookmarks(this.getBookmarks(xml), objectXml['elements'][0]['elements'][0]['elements']);
+            const bookmarksXml = documentData.bookmarks;
+            let paragraphsXml = documentData.paragraphs;
 
             for (const bookmarkXml of bookmarksXml) {
                 if (!bookmarkXml.name) {
@@ -160,6 +224,8 @@ export default class DocumentMerger {
                 if (!mainBookmark) {
                     continue;
                 }
+
+                paragraphsXml = await this.computingLinks(bookmarkXml.start, bookmarkXml.end, paragraphsXml, objectLinksXml);
 
                 if (bookmarkXml.countParagraphs === mainBookmark.countParagraphs) {
                     for (let j = 0; j < mainBookmark.countParagraphs; j++) {
@@ -367,13 +433,16 @@ export default class DocumentMerger {
         let paragraphIsOpen = false;
         let bookMarkIsOpen = false;
         let countParagraphs = -1;
+        let fix_ = -1;
         for (const element of splitDocument) {
             if (element.includes('w:p ')) {
                 countParagraphs++;
+                fix_ = countParagraphs;
                 paragraphIsOpen = true;
                 bookMarkIsOpen = false;
             } else if (element.includes('/w:p>')) {
                 paragraphIsOpen = false;
+                fix_ += 1;
             }
 
             if (element.includes('w:bookmarkStart ')) {
@@ -400,8 +469,8 @@ export default class DocumentMerger {
 
                     const countParagraphsInBK = (indexEnd - bookmarks[id].start) + 1;
                     bookmarks[id].end = indexEnd;
-                    bookmarks[id].endInOtherPR = prBkIsOpen;
-                    bookmarks[id].endIsMinePR = (paragraphIsOpen && bookMarkIsOpen === false);
+                    bookmarks[id].endInOtherPR = (prBkIsOpen && (bookmarks[id].start != fix_));
+                    bookmarks[id].endIsMinePR = ((paragraphIsOpen && bookMarkIsOpen == false) || (bookmarks[id].start == fix_));
                     bookmarks[id].countParagraphs = countParagraphsInBK;
                 }
             }
@@ -440,22 +509,122 @@ export default class DocumentMerger {
         return NewParagraph;
     }
 
+    // /**
+    //  * Заменить тег w:r в paragraph на тот что в otherParagraph
+    //  * @param {{}} paragraph
+    //  * @param {{}} otherParagraph
+    //  * @returns {{}} Возвращает paragraph
+    //  */
+    // private replaceTextFromParagraph(paragraph: any[], otherParagraph: any[]): any[] {
+    //     if (
+    //         !paragraph ||
+    //         !otherParagraph ||
+    //         !otherParagraph['w:r'] ||
+    //         !paragraph['w:r']
+    //     ) {
+    //         return paragraph;
+    //     }
+    //     paragraph['w:r'] = otherParagraph['w:r'];
+    //     return paragraph;
+    // }
+
+
     /**
-     * Заменить тег w:r в paragraph на тот что в otherParagraph
+     * Заменить тэги в paragraph на те что в otheParagraph кроме bookmarkStart,bookmarkEnd
      * @param {{}} paragraph
-     * @param {{}} otherParagraph
+     * @param {{}} otheParagraph
+     * @param check
      * @returns {{}} Возвращает paragraph
      */
-    private replaceTextFromParagraph(paragraph: any[], otherParagraph: any[]): any[] {
-        if (
-            !paragraph ||
-            !otherParagraph ||
-            !otherParagraph['w:r'] ||
-            !paragraph['w:r']
-        ) {
-            return paragraph;
+    replaceTextFromOtherParagraph(paragraph: convert.Element, otheParagraph: convert.Element, check: boolean): convert.Element {
+        let buffer = [];
+        const parhLength = paragraph['elements'].length;
+        const otherParLength = otheParagraph['elements'].length;
+
+        for(let i = 0; i < parhLength; i++) {
+            const elementName = paragraph['elements'][i]['name'];
+            if (!(elementName === 'w:bookmarkEnd' || elementName === 'w:bookmarkStart' || elementName === 'w:pPr')) {
+                break;
+            } else {
+                if (elementName === 'w:pPr') {
+                    const _ind = otheParagraph['elements'].findIndex(i => i['name'] == 'w:pPr');
+                    if (_ind > -1) {
+                        buffer.push(otheParagraph['elements'][_ind]);
+                    } else {
+                        buffer.push(paragraph['elements'][i]);
+                    }
+                } else {
+                    buffer.push(paragraph['elements'][i]);
+                }
+            }
         }
-        paragraph['w:r'] = otherParagraph['w:r'];
+
+        for(let i = 0; i < otherParLength; i++) {
+            const elementName = otheParagraph['elements'][i]['name'];
+            if(!(elementName === 'w:bookmarkEnd' || elementName === 'w:bookmarkStart' || elementName === 'w:pPr')) {
+                buffer.push(otheParagraph['elements'][i]);
+            }
+        }
+
+        for(let i = parhLength-1; i > 0; i--) {
+            const _name = paragraph['elements'][i]['name'];
+
+            if(_name == 'w:bookmarkEnd') {
+
+                let _id: string | number = -1;
+                let _ind = -1;
+
+                try {
+                    _id = paragraph['elements'][i]['attributes']['w:id'];
+                } catch (error) {
+                    console.error(`[ReplaceTextFromOtherParagraph | id] ${error}`);
+                    _id = -1;
+                }
+
+                try {
+                    _ind = buffer.findIndex(i => (i['attributes']['w:id'] === null || i['attributes']['w:id'] === undefined ? '' : i['attributes']['w:id']) == _id);
+                } catch (error) {
+                    _ind = -2;
+                }
+
+                if(_ind === -1 || check) {
+                    buffer.push(paragraph['elements'][i]);
+                }
+            } else {
+                break;
+            }
+        }
+
+        paragraph['elements'] = buffer;
+
+        return paragraph;
+    }
+
+    /**
+     * Удалить все тэги кроме bookmarkEnd,bookmarkStart,pPr в параграфе
+     * @param {{}} paragraph
+     * @returns {{}} Возвращает paragraph
+     */
+    deleteTextInParagraph(paragraph: convert.Element): convert.Element {
+        let buffer = [];
+        for(const element of paragraph['elements']) {
+            const _name = element.name;
+            if(_name === 'w:bookmarkEnd' || _name === 'w:bookmarkStart' || _name === 'w:pPr') {
+                buffer.push(element);
+            }
+        }
+        paragraph['elements'] = buffer;
+        return paragraph;
+    }
+
+    deleteBookMarksInLastPR_FIX(paragraph: convert.Element): convert.Element {
+        for(const [i, element] of paragraph['elements'].entries()) {
+            const _name = element.name;
+            if(_name == 'w:bookmarkEnd') {
+                paragraph['elements'][i] = null;
+                paragraph['elements'].splice(i,1);
+            }
+        }
         return paragraph;
     }
 }
